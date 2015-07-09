9dcbb89fc43bdc7ec1085fc0fa8c2e8b5d6114ec
Karl <khughes.me@gmail.com>
2015-07-09 09:03:48 -0500

    Adding last test to valore price client

--- test/Clients/ValoreBooksPriceClientTest.php
+++ test/Clients/ValoreBooksPriceClientTest.php
@@ -108,7 +108,6 @@ class ValoreBooksPriceClientTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals([], $this->client->collection);
     }
 
-    /*
     public function testItCreatesPriceCollectionFromApi()
     {
         $guzzleResponse = m::mock();
@@ -128,25 +127,46 @@ class ValoreBooksPriceClientTest extends \PHPUnit_Framework_TestCase
 
     private function generateXmlResponse()
     {
-        return '<?xml version="1.0" encoding="utf-8"?>
-            <CheggAPIResponse>
-                <Items><Item><BookInfo>
-                    <Title>Abbreviated Injury Scale (Ais) 1990 - Update 98</Title>
-                    <EditionNumber>1</EditionNumber>
-                    <EditionType></EditionType>
-                    <PubDate></PubDate>
-                    <PubName>Association for the Advancement of Baltic Studies</PubName>
-                    <ListPrice>235.29</ListPrice>
-                    <Authors/>
-                    <ImageThumb>http://c.cheggcdn.com/covers2/imagena.gif</ImageThumb>
-                    <ImageSmall>http://c.cheggcdn.com/covers2/imagenalarge.gif</ImageSmall>
-                    <ImageMedium>http://c.cheggcdn.com/covers2/imagenalarge.gif</ImageMedium>
-                    <ImageLarge>http://c.cheggcdn.com/covers2/imagenalarge.gif</ImageLarge></BookInfo><ISBN>0000002003</ISBN><EAN>9780000002006</EAN><Terms><Term><Price>9.99</Price><Id>157</Id><Term>SEMESTER</Term><Name>Semester Rental</Name><Term_days>164</Term_days><Due_date>2015-12-18</Due_date><Orig_price>9.99</Orig_price><Discount_info>0</Discount_info><Pid>LBP-980139|867189a2-ef39-490e-9ed6-c42624bbdd16|1</Pid></Term><Term><Price>9.99</Price><Id>157</Id><Term>QUARTER</Term><Name>Quarter Rental</Name><Term_days>164</Term_days><Due_date>2015-12-18</Due_date><Orig_price>9.99</Orig_price><Discount_info>0</Discount_info><Pid>LBP-980139|867189a2-ef39-490e-9ed6-c42624bbdd16|1</Pid></Term><Term><Price>9.99</Price><Id>157</Id><Term>SUMMER</Term><Name>Summer Rental</Name><Term_days>164</Term_days><Due_date>2015-12-18</Due_date><Orig_price>9.99</Orig_price><Discount_info>0</Discount_info><Pid>LBP-980139|867189a2-ef39-490e-9ed6-c42624bbdd16|1</Pid></Term></Terms><Renting>1</Renting><ShippingPrices><ShippingPrice><Method_name>Ground Shipping</Method_name><Cost_first>5.99</Cost_first><Cost_each>2.99</Cost_each><Guarantee_date>07-15-2015</Guarantee_date></ShippingPrice><ShippingPrice><Method_name>Standard</Method_name><Cost_first>4.99</Cost_first><Cost_each>3.99</Cost_each><Guarantee_date>07-17-2015</Guarantee_date></ShippingPrice><ShippingPrice><Method_name>UPS 3rd Day</Method_name><Cost_first>5.99</Cost_first><Cost_each>2.99</Cost_each><Guarantee_date>07-11-2015</Guarantee_date></ShippingPrice><ShippingPrice><Method_name>UPS 2nd Day</Method_name><Cost_first>10.99</Cost_first><Cost_each>5.99</Cost_each><Guarantee_date>07-10-2015</Guarantee_date></ShippingPrice><ShippingPrice><Method_name>UPS Next Day Air</Method_name><Cost_first>16.99</Cost_first><Cost_each>10.99</Cost_each><Guarantee_date>07-08-2015</Guarantee_date>
-                    </ShippingPrice></ShippingPrices>
-                    </Item></Items>
-            </CheggAPIResponse>';
+        return '<?xml version="1.0" encoding="UTF-8"?>
+             <product>
+             <product-code>9780000000071</product-code>
+
+             <sale-offer>
+             <price>0.01</price>
+             <condition>Like New</condition>
+             <alternate-edition>false</alternate-edition>
+             <link>http://www.valorebooks.com/listing?product_id=5791026&amp;sellerMarket_id=1196849012&amp;a_listing=false&amp;site_id=Og3W1I</link>
+             <quantity>1</quantity>
+             <shipping-options>
+             <shipping>
+             <method>Standard</method>
+             <price-first>3.95</price-first>
+             </shipping>
+             <shipping>
+             <method>Expedited</method>
+             <price-first>6.95</price-first>
+             </shipping>
+             </shipping-options>
+             </sale-offer></product><?xml version="1.0" encoding="UTF-8"?>
+             <product>
+             <product-code>9780001203020</product-code></product><?xml version="1.0" encoding="UTF-8"?>
+             <product>
+             <product-code>9780007205707</product-code>
+
+             <sale-offer>
+             <price>7.92</price>
+             <condition>New</condition>
+             <alternate-edition>false</alternate-edition>
+             <link>http://www.valorebooks.com/listing?product_id=9504430&amp;sellerMarket_id=1400024232&amp;a_listing=false&amp;site_id=Og3W1I</link>
+             <quantity>50</quantity>
+             <shipping-options>
+             <shipping>
+             <method>Standard</method>
+             <price-first>3.95</price-first>
+             </shipping>
+             </shipping-options>
+             </sale-offer></product>';
     }
-    */
 
     private function generateResponse($options = [])
     {
