37c8f5a38db3f788c68220b12989bf5dccb787a0
Karl <khughes.me@gmail.com>
2015-07-10 11:53:15 -0500

    Adding skyo

---
+++ src/Clients/SkyoPriceClient.php
@@ -0,0 +1,32 @@
+<?php namespace Packback\Prices\Clients;
+
+class SkyoPriceClient extends CommissionJunctionPriceClient
+{
+    const RETAILER = 'skyo';
+
+    public function __construct($config = [])
+    {
+        $cj_config = array_merge($config['cj'], $config[self::RETAILER]);
+        parent::__construct($cj_config);
+    }
+
+    public function getPricesForIsbns($isbns = [])
+    {
+        foreach ($isbns as $isbn) {
+            $response = $this->addParam('isbn', $isbn)->send();
+            $this->addPricesToCollection($response);
+        }
+        $this->addMetaToPrices($this->collection);
+        return $this->collection;
+    }
+
+    private function addMetaToPrices($prices = [])
+    {
+        foreach($prices as $i => $price) {
+            $prices[$i]->retailer = self::RETAILER;
+            $prices[$i]->condition = parent::CONDITION_NEW;
+            $prices[$i]->term = parent::TERM_SEMESTER;
+        }
+        return $prices;
+    }
+}
---
+++ test/Clients/SkyoPriceClientTest.php
@@ -0,0 +1,81 @@
+<?php namespace Packback\Prices\Test;
+
+use Packback\Prices\Clients\SkyoPriceClient;
+use Mockery as m;
+
+class SkyoPriceClientTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        include 'config.php';
+        $this->client = new SkyoPriceClient($config);
+        $this->client->client = m::mock('CROSCON\CommissionJunction\Client');
+    }
+
+    public function testItCreatesPriceCollectionFromApi()
+    {
+        $isbn = uniqid();
+        $clientResponse = $this->generateResponse(1, ['isbn' => $isbn]);
+
+        $this->client->client->shouldReceive('productSearch')
+            ->andReturn($clientResponse);
+
+        $this->client->getPricesForIsbns([$isbn]);
+
+        foreach ($this->client->collection as $key => $book) {
+            $this->assertEquals($clientResponse->products->product->isbn, $book->isbn13 );
+            $this->assertEquals('skyo', $book->retailer);
+            $this->assertEquals('new', $book->condition);
+            $this->assertEquals('semester', $book->term);
+        }
+    }
+
+    private function generateResponse($count = 1, $params = [])
+    {
+        $response = [
+            'products' => [
+                '@attributes' => [
+                    'total-matched' => $count,
+                    'records-returned' => $count,
+                    'page-number' => '1',
+                ],
+            ]
+        ];
+        if ($count === 1) {
+            $response['products']['product'] = $this->generateProductResponse($params);
+        } else {
+            $cc = 0;
+            while ($cc < $count) {
+                $response['products']['product'][] = $this->generateProductResponse($params);
+                $cc++;
+            }
+        }
+        return json_decode(json_encode($response));
+    }
+
+    private function generateProductResponse($params = [])
+    {
+        $product = [
+            'ad-id' => uniqid(),
+            'isbn' => uniqid(),
+            'advertiser-id' => uniqid(),
+            'advertiser-name' => uniqid(),
+            'advertiser-category' => uniqid(),
+            'buy-url' => uniqid(),
+            'catalog-id' => uniqid(),
+            'currency' => uniqid(),
+            'description' => uniqid(),
+            'image-url' => uniqid(),
+            'in-stock' => uniqid(),
+            'manufacturer-name' => [],
+            'manufacturer-sku' => [],
+            'name' => uniqid(),
+            'price' => uniqid(),
+            'retail-price' => [],
+            'sale-price' => [],
+            'sku' => uniqid(),
+        ];
+        $product = array_replace($product, $params);
+        return $product;
+    }
+}
